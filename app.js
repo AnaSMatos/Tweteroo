9e414ad55f3e29fc953450c8ca4690e3035bfaa7
Ana Clara <anasmatos28@gmail.com>
2022-04-25 15:39:16 -0300

    add + bonus

--- app.js
+++ app.js
@@ -10,21 +10,32 @@ app.use(express.json())
 let user = [];
 let tweets = [];
 let currentAvatar;
+let currentUser;
 
 app.post("/sign-up", (req, res) => {
     user.push(req.body);
     currentAvatar = req.body.avatar;
+    currentUser = req.body.username;
     res.status(201).send("OK")
 })
 
 app.post("/tweets", (req, res) => {
-    tweets.push({...req.body, avatar: currentAvatar});
+    tweets.push({...req.body, avatar: currentAvatar, username: currentUser});
     res.status(201).send("OK")
 })
 
 app.get("/tweets", (req, res) => {
     let lastTweets = tweets.slice(-10);
     res.send(lastTweets.reverse())
+})
+
+app.get("/tweets/:user", (req, res) => {
+    user = req.params.user
+    let userTweets = tweets.filter(tweet => {
+        return tweet.username === user;
+    })
+
+    res.send(userTweets);
 })
 
 app.listen(port)
